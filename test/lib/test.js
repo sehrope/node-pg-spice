3c8fd507b2be032f68434a757c1a513bd0313d98
Sehrope Sarkuni <sehrope@jackdb.com>
2014-05-20 15:49:42 -0400

    Replace pg dependency with pg.js

--- test/lib/test.js
+++ test/lib/test.js
@@ -1,10 +1,10 @@
 module.exports = function(options) {
 	// Delete from cache as the monkey patch will not apply itself twice:
-	delete require.cache[require.resolve('pg')]
+	delete require.cache[require.resolve('pg.js')]
 	delete require.cache[require.resolve('../../index.js')]
 
 	// Require pg and pg-spice:
-  var pg = require('pg');
+  var pg = require('pg.js');
   var spice = require('../../index.js');
 
   // Patch with the specified options:
